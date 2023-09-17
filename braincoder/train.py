ecc910e4ccc904f58e32c31c27bb9f171fe45b71
yoonhero <yoonhero06@naver.com>
2023-09-17 18:43:50 +0900

    Training Pipeline planning

--- braincoder/train.py
+++ braincoder/train.py
@@ -17,15 +17,29 @@ models = {
     "attention": None
 }
 
-class LinearModel(LightningModule):
+learning_rate = 1e-3
+batch_size = 64
+epochs = 20
+
+class Model(LightningModule):
     def __init__(self, model_name, lr,**kwargs):
         super().__init__()
-
         self.learning_rate = lr
+
+        self.model_name = model_name
         self.model = models[model_name](**kwargs)
+
         self.criterion = nn.KLDivLoss(reduction="batchmean")
         
     def forward(self, x):
+        if self.model_name in ["cnn", "attention"]:
+            # for cnn input process is required
+            # (B, 14, 640, 480, 3)
+            # (B, 14*3, 640, 480)
+            # consider eeg channel with image channel
+            # experiment the effect of these concat(?)
+            pass
+        
         return self.model(x)
 
     def loss_term(self, y_hat, y):
@@ -57,3 +71,17 @@ class LinearModel(LightningModule):
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+
+model_name = "linear"
+## Add additional arguments for respective networks
+model = Model(model_name, learning_rate)
+
+
+trainer = Trainer(max_epochs=epochs, gpus=0)
+trainer.fit(model, train_dataloaders, val_dataloaders)
+
+trainer.test(test_dataloader)
+
+model.eval()
+### Evaluation step
