40c9fdda45a39504297f3d28701b67e731f011fe
yoonhero <yoonhero06@naver.com>
2023-10-23 15:49:26 +0900

    wellinitialize

--- train.py
+++ train.py
@@ -165,7 +165,7 @@ def train():
                 # Saving sample for visualizing the result
                 for i in range(how_many_to_save):
                     print(yhat.shape)
-                    if i<yhat.shape[0]:
+                    if i>yhat.shape[0]:
                         continue
                     pred = yhat[i].cpu().detach()
                     im_key = im_keys[i].item()
