d9a359018bb04d31f1620a2cedabbd8aa3011cf3
yoonhero <yoonhero06@naver.com>
2023-10-26 18:29:06 +0900

    whwywywywhwhywwy

--- model.py
+++ model.py
@@ -36,7 +36,40 @@ class LinearModel(nn.Module):
 
         x = x.view(B, 77, 768)
 
-        return x
+
+# class Fuckk(nn.Module):
+#     def __init__(self, eeg_channels, w=640, h=480, channels=42):
+#         super().__init__()  
+#         assert hidden[-1] == 77*768, "Please check the hidden state size."
+
+#         self.eeg_c = eeg_channels
+#         self.w = w
+#         self.h = h
+#         self.c = channels
+
+#         self.initial_linear = nn.Linear(channels*w*h, hidden[0])
+#         layers = []
+#         for (fan_in, fan_out) in zip(hidden, hidden[1:]):
+#             layers.append(nn.Linear(fan_in, fan_out))
+
+#         self.linears = nn.ModuleList(**layers)        
+#         self.relu = nn.ReLU()
+
+        
+    
+#     def forward(self, x):
+#         # (Batch, 14, 640, 480, 3)
+#         B = x.size(0)
+#         x = x.view(B, self.eeg_c, -1)
+
+#         x = self.relu(self.initial_linear(x))
+    
+#         for layer in self.layers:
+#             x = layer(x)
+#             x = self.relu(x)
+
+#         x = x.view(B, 77, 768)
+
 
 
 def conv_3x3_bn(inp, oup, image_shape, downsample=False):
